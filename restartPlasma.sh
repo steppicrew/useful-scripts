a370f8762e2a423b412812ece7130e23bbce11cc
stephan hantigk <s.hantigk@runlevel3.de>
2017-03-16 20:49:19 +0100

    some more quoting start kwin_x11 as correct user

--- restartPlasma.sh
+++ restartPlasma.sh
@@ -5,18 +5,23 @@
 # It gets the user name as parameter or tries to find kwin for the current user
 
 user="$1"
+currentUser="`whoami`"
 
-test -z "$user" && user=`whoami`
+test -z "$user" && user="$currentUser"
 
-pid=`pgrep -u "$user" kwin_x11`
+pid="`pgrep -u "$user" kwin_x11`"
 
-test "$pid" || (echo "Could not get PID of kwin_x11 for user $user"; exit)
+if test -z "$pid" ; then
+    echo "Could not get PID of kwin_x11 for user $user"
+    exit
+fi
 
 cat "/proc/$pid/cmdline" | perl -e '
     $_=<STDIN>;
     my @cmd= split /\000/;
     exit unless @cmd;
-    kill "KILL", '$pid';
+    unshift @cmd, "su", "-", "'"$user"'" unless "'"$user"'" eq "'"$currentUser"'";
+    kill "KILL", '"$pid"';
     exec @cmd;
 #    print join ":", @cmd;
 ' &
