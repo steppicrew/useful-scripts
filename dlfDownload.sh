f7a25de97b049d22fcd031ef3b13e5ef5c7e1622
stephan hantigk <s.hantigk@runlevel3.de>
2020-02-17 09:40:15 +0100

    added script to download audio from dlf.de

---
+++ dlfDownload.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+pageUrl="$1"
+
+if [ -z "$pageUrl" ]; then
+    echo "Die Seite des dlf als Parameter angeben!"
+    exit
+fi
+
+masterFile=`wget -O - -q "$pageUrl" | perl -ne 'print $1 if /<meta name="twitter:player:stream" content="([^"]+)">/'`
+m3uUrl=`wget -O - -q "$masterFile" | perl -ne 'print $1 if /^(http.+)\s*$/'`
+
+baseUrl=`dirname "$m3uUrl"`
+outFile=`basename "$baseUrl"`
+
+m3uFile="$outFile.m3u"
+wget -O "$m3uFile" "$m3uUrl" || exit
+
+test -f "$outFile.m2t" && rm "$outFile.m2t"
+
+while read line; do
+    start="${line:0:1}"
+    test "$start" = '#' && continue
+    wget -O "$outFile.tmp" "$line"
+    cat "$outFile.tmp" >> "$outFile.m2t"
+done < "$m3uFile"
+
+ffmpeg -i "$outFile.m2t" -codec copy "$outFile"
+
+test -f "$m3uFile" && rm "$m3uFile"
+test -f "$outFile.tmp" && rm "$outFile.tmp"
