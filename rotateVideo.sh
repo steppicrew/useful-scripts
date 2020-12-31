49ddc4f4ff22c2cd39e89ee428e5801b0a1272ad
steppicrew <github@steppicrew.de>
2020-12-31 23:35:19 +0100

    Added rotate by 180

--- rotateVideo.sh
+++ rotateVideo.sh
@@ -6,10 +6,12 @@ file="$2"
 test ! "$rotdir" -o ! -r "$file" && echo "Usage: $0 [1|-1] [file]" && exit
 
 test "$rotdir" = "-1" && rotdir=2
+param="transpose=$rotdir"
+test "$rotdir" = "3" && param="transpose=2,transpose=2"
 
 dir="`dirname "$file"`"
 bname="`basename "$file"`"
 extension="${bname##*.}"
 bname="`basename "$file" ".$extension"`"
 
-ffmpeg -i "$file" -vf "transpose=$rotdir" "$dir/$bname-$rotdir.$extension"
+ffmpeg -i "$file" -vf "$param" "$dir/$bname-$rotdir.$extension"
